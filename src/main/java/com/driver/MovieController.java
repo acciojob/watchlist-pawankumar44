ca0acf23f74879e2fe9f1d31ec35dff512af9608
Pawan Kumar <pawankumar98>
2023-10-28 17:07:30 +0530

    working on assignment

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -19,6 +19,48 @@ public class MovieController {
         return "Movie added";
     }
 
+    @PostMapping("/movies/add-movie")
+    public String addDirector(@RequestBody Movie movie){
+        movieService.addMovie(movie);
+        return "Movie added";
+    }
+
+    @PostMapping("/movies/add-movie")
+    public String addMovieDirectorPair(@RequestBody Movie movie){
+        movieService.addMovie(movie);
+        return "Movie added";
+    }
+
+    @PostMapping("/movies/add-movie")
+    public String getMovieByName(@RequestBody Movie movie){
+        movieService.addMovie(movie);
+        return "Movie added";
+    }
+
+    @PostMapping("/movies/add-movie")
+    public String getDirectorByName(@RequestBody Movie movie){
+        movieService.addMovie(movie);
+        return "Movie added";
+    }
+
+    @PostMapping("/movies/add-movie")
+    public String findAllMovies(@RequestBody Movie movie){
+        movieService.addMovie(movie);
+        return "Movie added";
+    }
+
+    @PostMapping("/movies/add-movie")
+    public String deleteDirectorByName(@RequestBody Movie movie){
+        movieService.addMovie(movie);
+        return "Movie added";
+    }
+
+    @PostMapping("/movies/add-movie")
+    public String deleteAllDirectors(@RequestBody Movie movie){
+        movieService.addMovie(movie);
+        return "Movie added";
+    }
+
     @GetMapping("/movies")
     public String test(){
         return "Hi testing";
